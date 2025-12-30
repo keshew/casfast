6a507d8e4e5816e4e03f26d92a1e7077aa86d264
keshew <artyom.korotckov2017@yandex.ru>
2025-12-30 16:18:18 +0500

    Initial Commit

---
+++ casfast/casfastApp.swift
@@ -0,0 +1,17 @@
+//
+//  casfastApp.swift
+//  casfast
+//
+//  Created by Артём Коротков on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct casfastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
